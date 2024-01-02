70d99082f40977b37e7f3259ce5543161f617d28
Gordon Beeming <gpg@gordonbeeming.com>
2024-01-02 17:23:30 +1000

    run tests

--- Program.cs
+++ Program.cs
@@ -1,22 +1,3 @@
-﻿using System.Text;
+﻿using BenchmarkDotNet.Running;
 
-var name = "Gordon";
-var message = "Hello, " + name + "!";
-var message2 = string.Format("Hello, {0} {1}!", name, DateTime.Now);
-var message3 = $"Hello, {name}!";
-
-var messageBuilder = new StringBuilder();
-messageBuilder.Append("Hello, ");
-messageBuilder.Append(name);
-messageBuilder.Append("!");
-
-messageBuilder.AppendLine();
-
-
-System.Console.WriteLine(message);
-
-
-
-
-
-
+var summary = BenchmarkRunner.Run<MyBenchmark>();
